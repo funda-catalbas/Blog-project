ecbe8505389336081a2a963b35d6c3767dbcaadd
funda-catalbas <funda.catalbas@gmail.com>
2023-01-26 13:09:19 +0300

    cardstyled

---
+++ js/general.js
@@ -0,0 +1,9 @@
+/*blog date start*/
+
+const blogDate=document.getElementById("blogDate")
+
+const newDate=new Date()
+
+blogDate.innerText=newDate.toLocaleDateString().replaceAll(".", "/")
+
+/*blog date end*/
